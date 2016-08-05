070f689b3a9fb0ff208d53514db39b178590f2e5
Cosmin <c0sm1n87@yahoo.co.uk>
2016-08-06 02:29:29 +0300

    vbim maine pe slack

--- azera-shop-luxury/functions.php
+++ azera-shop-luxury/functions.php
@@ -11,3 +11,20 @@ function azera_shop_luxury_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'azera_shop_luxury_enqueue_styles', 12 );
+
+function azera_shop_luxury_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'azera-shop' ),
+			'id'            => 'sidebar-1',
+			'description'   => '',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+}
+add_action( 'widgets_init', 'azera_shop_luxury_widgets_init', 12 );
