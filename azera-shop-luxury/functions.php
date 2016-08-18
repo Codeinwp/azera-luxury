c81ef8521ed808cb41d5617fde22755afc66209f
Cosmin <c0sm1n87@yahoo.co.uk>
2016-08-18 16:26:28 +0300

    commit

--- azera-shop-luxury/functions.php
+++ azera-shop-luxury/functions.php
@@ -28,3 +28,56 @@ function azera_shop_luxury_widgets_init() {
 
 }
 add_action( 'widgets_init', 'azera_shop_luxury_widgets_init', 12 );
+
+
+/**
+ * Custom functions to retrieve data
+ */
+function azera_luxury_numeric_posts_nav( $pages = '', $range = 4 ) {
+	$showitems = ( $range * 2 ) + 1;
+
+	global $paged;
+	if ( empty( $paged ) ) {
+		$paged = 1;
+	}
+
+	if ( $pages == '' ) {
+		global $wp_query;
+		$pages = $wp_query->max_num_pages;
+		if ( ! $pages ) {
+			$pages = 1;
+		}
+	}
+
+	if ( 1 != $pages ) {
+		echo '<div class="row text-center"><ul class="azera-luxury-pager">';
+		if ( $paged > 2 && $paged > $range + 1 && $showitems < $pages ) {
+			echo "<li><a href='" . get_pagenum_link( 1 ) .
+			     "'>1</a></li>";
+		}
+		if ( $paged > 1 && $showitems < $pages ) {
+			echo "<li><a href='" . get_pagenum_link( $paged - 1 ) .
+			     "'> ... </a></li>";
+		}
+
+		for ( $i = 1; $i <= $pages; $i ++ ) {
+			if ( 1 != $pages && ( ! ( $i >= $paged + $range + 1 || $i <= $paged - $range - 1 ) || $pages <= $showitems ) ) {
+				echo ( $paged == $i ) ? "<li class=\"active\">" . $i .
+				                        "</li>" : "<li><a href='" . get_pagenum_link( $i ) .
+				                                  "' class=\"inactive\">" . $i .
+				                                  "</a></li>";
+			}
+		}
+
+		if ( $paged < $pages && $showitems < $pages ) {
+			echo "<li><a href=\"" . get_pagenum_link( $paged + 1 ) .
+			     "\"> ... </a></li>";
+		}
+		if ( $paged < $pages - 1 && $paged + $range - 1 < $pages && $showitems < $pages ) {
+			echo "<li><a href='" . get_pagenum_link( $pages ) .
+			     "'>" . ($i - 1) . "</a></li>";
+		}
+		echo "</ul></div>\n";
+
+	}
+}
