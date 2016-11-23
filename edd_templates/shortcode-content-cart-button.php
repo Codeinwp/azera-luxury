3f570d664f5b8fc33c87d38bef8f4c44d723e952
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2016-11-23 16:52:30 +0200

    Removed overriden files from child theme and used hooks and filters

--- edd_templates/shortcode-content-cart-button.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! edd_has_variable_prices( get_the_ID() ) ) : ?>
-	<div class="azera_edd_price">
-		<?php echo edd_get_currency() . ' ' . edd_get_download_price( get_the_ID() ) ?>
-	</div>
-<?php endif; ?>
-<div class="edd_download_buy_button">
-	<?php echo edd_get_purchase_link( array( 'download_id' => get_the_ID(), 'price' => false ) ); ?>
-</div>
